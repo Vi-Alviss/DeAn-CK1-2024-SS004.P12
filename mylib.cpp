79ece995f305f0db23d3e5e4121542ac02fa9806
nhatquang <lenhatquang11a1@gmail.com>
2024-11-10 00:23:52 +0700

    them file mylib.cpp cua thu vien can thiet

---
+++ mylib.cpp
@@ -0,0 +1,63 @@
+#include "mylib.h"
+
+int whereX() // lay toa do X
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+        return csbi.dwCursorPosition.X;
+    return -1;
+}
+
+int whereY() // lay toa do y
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+        return csbi.dwCursorPosition.Y;
+    return -1;
+}
+
+void gotoXY(int x, int y) // di chuyen con tro den toa do (x,y)
+{
+    HANDLE hConsoleOutput;
+    COORD Cursor_an_Pos = {x, y};
+    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hConsoleOutput, Cursor_an_Pos);
+}
+
+void SetColor(WORD color) // dat mau cho chu
+{
+    HANDLE hConsoleOutPut;
+    hConsoleOutPut = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+    GetConsoleScreenBufferInfo(hConsoleOutPut, &screen_buffer_info);
+
+    WORD wAttributes = screen_buffer_info.wAttributes;
+    color &= 0x000f;
+    wAttributes &= 0xfff0;
+    wAttributes |= color;
+
+    SetConsoleTextAttribute(hConsoleOutPut, wAttributes);
+}
+
+void ShowCur(bool CursorVisibility) // lam an con tro chuot
+{
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cursor = {1, CursorVisibility};
+    SetConsoleCursorInfo(handle,&cursor);
+}
+
+int inputKey()
+{
+    if (_kbhit())
+    {
+        int key = _getch();
+
+        if (key == 224)
+        {
+            key = _getch();
+            return key + 1000;
+        }
+
+        return key;
+    }
+}
