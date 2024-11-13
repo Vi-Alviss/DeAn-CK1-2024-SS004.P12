7e345c965b4e9693e813c01171d85cb9777060cb
Vi-Alviss <23521791@gm.uit.edu.vn>
2024-11-13 15:28:40 +0700

    include <vector> into mylib.h

--- mylib.h
+++ mylib.h
@@ -2,10 +2,14 @@
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
+#include <vector>
 #define KEY_NONE -1
+
 #define Width 50
 #define Height 25
+
 using namespace std;
+
 int whereX();
 int whereY();
 void GetXY(int p_X, int p_Y);
