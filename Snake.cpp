495f1b3b7dbed3a8edc403af8805dde8f3b92708
LQH <lqhuy64@gmail.com>
2024-11-13 16:19:04 +0700

    Revert "add Move_Snake function"
    This reverts commit 6a89a9a59e2a6cfab250a0ed9435687eef36d993.
    
    fixed some errors

--- Snake.cpp
+++ Snake.cpp
@@ -1,20 +1,3 @@
 #include "Snake.h"
-void Snake::Move_Snake(bool grow = false)
-{
-    COORD newhead = {(SHORT)(object[0].X + (SHORT)dx), (SHORT)(object[0].Y + (SHORT)dy)};
 
-    // Check Collision with wall
-    if (newhead.X < 1)
-        newhead.X = Width;
-    else if (newhead.X > Width)
-        newhead.X = 1;
-    if (newhead.Y < 1)
-        newhead.Y = Height;
-    else if (newhead.Y > Height)
-        newhead.Y = 1;
-    object.insert(object.begin(), newhead);
-    if (!grow)
-    {
-        object.pop_back();
-    }
-}
+
