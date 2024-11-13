a7f72ae9794c402d02aab6dedf983d57204894d0
blastvious <silverhd280105@gmail.com>
2024-11-13 17:06:46 +0700

    Update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -48,6 +48,15 @@ bool Snake::CollisionBody()
 }
 bool Snake:: Eat_food(Food &p_food, int &mark)
 {
+<<<<<<< Updated upstream
+=======
+    object.push_back({50 / 2, 25 / 2});
+    object.push_back({50 / 2 - 1, 25 / 2});
+    object.push_back({50 / 2 - 2, 25 / 2});
+}
+bool Snake:: Eat_food(Food &p_food, int &mark)
+{
+>>>>>>> Stashed changes
     if (object[0].X == p_food.Pos_food.X && object[0].Y == p_food.Pos_food.Y)
     {
         p_food.Respawn_food();
