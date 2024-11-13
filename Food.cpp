1a4d153c193187ffc34fdb39daebf35d96f65dec
blastvious <silverhd280105@gmail.com>
2024-11-13 15:01:31 +0700

    Food

---
+++ Food.cpp
@@ -0,0 +1,18 @@
+#include "Food.h"
+Food::Food()
+{
+    point = 10;
+    Food::Respawn_food();
+}
+
+void Food::Respawn_food()
+{
+    Pos_food.X = (rand() % Width) + 1;
+    Pos_food.Y = (rand() % Height) + 1;
+}
+
+void Food::Draw_Food()
+{
+    GetXY(Pos_food.X, Pos_food.Y);
+    cout << "O";
+}
