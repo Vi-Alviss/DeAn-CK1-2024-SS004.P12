ae67c6665b0b5fdcc1a8e1213456a5b7cb02eeb0
nhatquang <lenhatquang11a1@gmail.com>
2024-11-13 16:55:22 +0700

    Revert "Create Snake"
    This reverts commit 51b233d2a2eb9527e71cac9f8d7d57558dae9275.
    restore previous verion

--- Snake.cpp
+++ Snake.cpp
@@ -46,19 +46,3 @@ bool Snake::CollisionBody()
     }
     return false;
 }
-
-Snake::Snake() : dx(1), dy(0)
-{
-    object.push_back({50 / 2, 25 / 2});
-    object.push_back({50 / 2 - 1, 25 / 2});
-    object.push_back({50 / 2 - 2, 25 / 2});
-}
-
-void Snake::Draw()
-{
-    for (const auto &pos : object)
-    {
-        GetXY(pos.X, pos.Y);
-        cout << "X";
-    }
-}
