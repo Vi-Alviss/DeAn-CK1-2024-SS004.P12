69e6ae79e7211bf1e958e41cee12f2dadae63d10
Vi-Alviss <23521791@gm.uit.edu.vn>
2024-11-13 14:54:27 +0700

    Reorganized code in main.cpp

--- main.cpp
+++ main.cpp
@@ -19,76 +19,6 @@ public:
     Point(int p_x, int p_y) : x(p_x), y(p_y) {}
 };
 
-class Food {
-public:
-    COORD Pos_food;
-    int score;
-    // Khoi tao thuc an
-    Food() {
-        score = 10;
-        Respawn_food();
-    }
-    //Sinh ngau nhien
-    void Respawn_food (){
-        Pos_food.X = rand() % Width;
-        Pos_food.Y = rand() % Height;
-    }
-    // Hien thi thuc an tren man hinh
-    void Draw_Food () const{
-        gotoXY(Pos_food.X,Pos_food.Y);
-        cout<<"O";
-    }
-
-};
-class Snake
-{
-public:
-    vector<COORD> object;
-    int dx, dy;
-    Snake() : dx(1), dy(0)
-    {
-        object.push_back({50 / 2, 25 / 2});
-        object.push_back({50/2 - 1, 25/2});
-        object.push_back({50/2 -2, 25/2});
-    }
-    void Draw()
-    {
-        for (const auto &pos : object)
-        {
-            gotoXY(pos.X, pos.Y);
-            cout << "X";
-        }
-        // GetXY(object.back().X, object.back().Y);
-        // cout << " ";
-    }
-        void Erase()
-    {
-        // Xóa toàn bộ con rắn (để vẽ lại sau)
-        for (const auto &pos : object)
-        {
-            gotoXY(pos.X, pos.Y);
-            cout << " ";
-        }
-    }
-    void Move_Snake (bool grow = false){
-        COORD newhead = { (SHORT)(object[0].X + (SHORT)dx), (SHORT)(object[0].Y + (SHORT)dy) };
-
-        object.insert(object.begin(), newhead);
-        if(! grow){
-            object.pop_back();
-        }
-    }
-    void Change_dir(int new_dx, int new_dy)
-    {
-        if (new_dx != -dx || new_dy != -dy){
-            dx = new_dx;
-            dy = new_dy;
-        }
-    }
-};
-
-
-
 bool Get_Key(Snake &p_snake)
 {
     if (_kbhit())
@@ -117,20 +47,5 @@ bool Get_Key(Snake &p_snake)
 
 int main()
 {
-    Snake s;
-    bool run = true;
-    srand(static_cast<unsigned int>(time(0)));
-    Food O_food;
-    while (run)
-    {
-        O_food.Respawn_food();
-        O_food.Draw_Food();
-        run = Get_Key(s);
-        s.Erase();
-        s.Move_Snake();
-        s.Draw();
-        Sleep(500);
-        /* code */
-    }
     return 0;
 }
