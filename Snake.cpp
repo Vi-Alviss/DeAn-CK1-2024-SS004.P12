2310721f3353da90ccd20dc7f34869fa0dc52de1
Vi-Alviss <23521791@gm.uit.edu.vn>
2024-11-13 16:22:27 +0700

    Add Erase function

--- Snake.cpp
+++ Snake.cpp
@@ -1,3 +1,12 @@
 #include "Snake.h"
 
 
+void Snake::Erase()
+{
+    // Xóa toàn bộ con rắn (để vẽ lại sau)
+    for (const auto &pos : object)
+    {
+        GetXY(pos.X, pos.Y);
+        cout << " ";
+    }
+}
